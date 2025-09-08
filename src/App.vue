4ffd890399153336b3a930a22d74c0f537ec2350
MJWade96 <2958594926@qq.com>
2025-09-08 15:44:51 +0800

    文件哈希值计算

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,13 @@
 // 定义切片大小为 200KB（可根据需求调整）
 const SIZE = 200 * 1024;
 
-function handleFile() {
-  
+// 工作线程
+const worker = new Worker(new URL('hash.js', import.meta.url));
+
+function handleFile(event) {
+  const file = event.currentTarget.files;
+  const fileChunkList = createFileChunk(file);
+  worker.postMessage({ fileChunkList });
 }
 
 // 生成文件切片
@@ -16,7 +21,7 @@ function createFileChunk(file, size = SIZE) {
     // --- 切片索引（index）和
     // --- 切片校验值（hash）
     fileChunkList.push({
-      file: file.slice(cur, cur + size), // 从当前位置截取到下一切片位置
+      chunk: file.slice(cur, cur + size), // 从当前位置截取到下一切片位置
     })
     cur += size // 移动到下一切片的起始位置
   }
---
+++ src/hash.js
@@ -0,0 +1,35 @@
+import SparkMD5 from 'spark-md5'
+
+self.onmessage = e => {
+    const { fileChunkList } = e.data
+    const spark = new SparkMD5.ArrayBuffer() // 创建SparkMD5实例
+    let percentage = 0 // 计算进度
+    const percentInterval = 100 / fileChunkList.length;
+    let count = 0 // 已处理的切片数量
+
+    // 开始处理第一个切片
+    loadNext(count)
+
+    // 递归处理所有切片
+    async function loadNext(index) {
+        const fileChunk = fileChunkList[index].chunk;
+        // 读取当前切片的ArrayBuffer
+        const chunkBuffer = await fileChunk.arrayBuffer();
+        // 将当前切片的内容添加到哈希计算中
+        spark.append(chunkBuffer);
+        count++
+        if (count < fileChunkList.length) {
+            // 计算当前进度并发送给主线程
+            percentage += percentInterval;
+            self.postMessage({ percentage })
+            loadNext(count) // 处理下一个切片
+        } else {
+            // 处理完所有切片后，计算最终hash
+            self.postMessage({
+                percentage: 100,
+                hash: spark.end() // 生成最终hash值
+            })
+            self.close() // 关闭Worker            
+        }
+    }
+}
